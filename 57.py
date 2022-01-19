7f249f3eb747e69b01db80d45dc78388c9ffcfc8
kombuchan <76601670+kombuchan@users.noreply.github.com>
2022-01-18 20:58:48 -0800

    create 57.py

---
+++ 57.py
@@ -0,0 +1,26 @@
+# Insert Interval
+
+class Solution:
+    def earlier_intervals(self,intervals,newInterval):
+        ret = []
+        for i in intervals:
+            if i[1] < newInterval[0]: ret.append(i)
+        return ret
+    
+    def later_intervals(self,intervals,newInterval):
+        ret = []
+        for i in intervals:
+            if i[0] > newInterval[1]: ret.append(i)
+        return ret
+    
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        left = self.earlier_intervals(intervals,newInterval)
+        right = self.later_intervals(intervals,newInterval)
+        
+        if left + right == intervals:
+            return left + [newInterval] + right
+        else:   
+            start = min(intervals[len(left)][0], newInterval[0])
+            end = max(intervals[-len(right)-1][1], newInterval[1])
+            return left+[[start,end]] +right
