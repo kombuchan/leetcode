3cc81d510d1e3022382da18fe3c40601f68efb2f
kombuchan <76601670+kombuchan@users.noreply.github.com>
2022-02-02 08:00:42 -0800

    Optimal solution

--- 347.py
+++ 347.py
@@ -5,25 +5,21 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numDict = {}
         for num in nums:
-            if num not in numDict: numDict[num] = 1
-            else: numDict[num] += 1
+            numDict[num] = numDict[num] + 1 if num in numDict else 1
+            
         
-        numCount = {}
-        for i in range(len(nums)+1): numCount[i] = []
+        numCount = [[] for i in range(len(nums)+1)]
+        
         
         for num in numDict:
             count = numDict[num]
             numCount[count].append(num)
         
         ret = []
-        
         i = len(nums)
-        while(len(ret)!=k) and i >= 0:
-            if len(numCount[i]) > 0:
-                j = 0
-                while len(ret) < k and j < len(numCount[i]): 
-                    ret.append(numCount[i][j])
-                    j += 1
-            i -= 1
+        for i in reversed(range(len(numCount))):
+            for j in numCount[i]:
+                 if len(ret) < k : ret.append(j)
+
         
         return ret
