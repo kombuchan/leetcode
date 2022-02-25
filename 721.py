1fa2dd8c04afff3daeaa5617d457320f75007ea3
kombuchan <76601670+kombuchan@users.noreply.github.com>
2022-02-24 19:03:25 -0800

    Create 721.py

---
+++ 721.py
@@ -0,0 +1,38 @@
+# Accounts Merge
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        users = {}
+        for num, i in enumerate(accounts):
+            name = i[0]
+            if name in users: users[name + "_dup" + str(num)] = set(i[1:])
+            else: users[name] = set(i[1:])
+        visited = set()
+        
+        print(users)
+        print("-----------------------")
+        
+        length,n = len(users),len(users)
+        
+        for i in users:
+            for email in users[i]:
+                if email in visited:
+                    original = i.split("_dup")[0]
+                    for j in users[i]: users[original].add(j)
+                    users[i] = ""
+                else: visited.add(email)
+                    
+        
+        print(users)
+        print("-----------------------")
+        
+        ret = []
+        for i in users:
+            name = i
+            if users[i] == "": continue
+            if "_dup" in name: 
+                name = i.split("_dup")[0]
+            ret.append([name] + sorted(list(users[i])))
+            
+        
+        return ret 
+        
