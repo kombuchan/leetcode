30e17362aa2692ddd28576e5a5307aae8af055bc
kombuchan <76601670+kombuchan@users.noreply.github.com>
2022-02-28 20:12:37 -0800

    Create 554.py

---
+++ 554.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        cracks = defaultdict(int)
+        for row in wall:
+            width = 0
+            for brick in range(len(row)):
+                width += row[brick]
+                if brick != len(row)-1:  cracks[width] += 1
+        
+        return len(wall) - max(cracks.values()) 
+    
