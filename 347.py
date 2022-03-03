518829a0b0de02950f129a82ac68490e7fe68d7c
kombuchan <76601670+kombuchan@users.noreply.github.com>
2022-03-03 12:09:04 -0800

    Top K Frequent Elements

--- 347.py
+++ 347.py
@@ -1,15 +1,12 @@
 # Top K Frequent Elements
-
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numDict = {}
         for num in nums:
             numDict[num] = numDict[num] + 1 if num in numDict else 1
-            
         
         numCount = [[] for i in range(len(nums)+1)]
-        
         
         for num in numDict:
             count = numDict[num]
@@ -21,5 +18,4 @@ class Solution:
             for j in numCount[i]:
                  if len(ret) < k : ret.append(j)
 
-        
         return ret
