34eea38fffb2208bfe3faa3325b1c739855a3c8d
Mihika Krishna <krishna.mihika@gmail.com>
2022-07-03 22:58:20 -0500

    Leetocde 739 Naive Solution

---
+++ 739.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            l = temperatures[i]
+            r = 1
+            isFound = 0
+            pos = i + 1
+            while (isFound != 1 and pos < len(temperatures)):
+                curr_temp = temperatures[pos]
+                if (l < curr_temp):
+                    ans[i] = r
+                    isFound = 1
+                r+=1
+                pos+=1
+        return ans
