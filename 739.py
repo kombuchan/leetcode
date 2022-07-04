aef078c06f52e35014f03d62379bb87964559161
Mihika Krishna <krishna.mihika@gmail.com>
2022-07-04 01:56:49 -0500

    739 Optimized soln

--- 739.py
+++ 739.py
@@ -1,17 +1,15 @@
 # 739. Daily Temperatures
+from collections import deque
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = deque()
         ans = [0]*len(temperatures)
         for i in range(len(temperatures)):
-            l = temperatures[i]
-            r = 1
-            isFound = 0
-            pos = i + 1
-            while (isFound != 1 and pos < len(temperatures)):
-                curr_temp = temperatures[pos]
-                if (l < curr_temp):
-                    ans[i] = r
-                    isFound = 1
-                r+=1
-                pos+=1
+            if i == 0: 
+                stack.append((temperatures[i],i))
+                continue
+            while stack and temperatures[i] > stack[-1][0]:
+                temp = stack.pop()
+                ans[temp[1]]= i - temp[1]
+            stack.append((temperatures[i],i))
         return ans
