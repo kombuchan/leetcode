1cfb3fc704e6dcccf67c9b62cc11648d5cbdd462
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-09-10 15:46:12 -0700

    Longest Consecutive Sequence

--- 621.py
+++
@@ -1,5 +0,0 @@
-from ast import List
-
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        return
--- 752.py
+++
@@ -1,5 +0,0 @@
-from ast import List
-
-class Solution:
-    def openLock(self, deadends: List[str], target: str) -> int:
-        return 0
