03c9ef39e17fdf482d88a091060e2fd4c083b14a
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-09-21 08:44:44 -0700

    Naive with memo

--- 70.py
+++ 70.py
@@ -1,12 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return self.dfs(n, 0, 0)
+        memo = {}
+        return self.dfs(n, 0, 0, memo)
     
-    def dfs(self, n, currSteps, numWays):
+    def dfs(self, n, currSteps, numWays, memo):
+        if currSteps in memo:
+            return memo[currSteps]
         if currSteps == n:
             return 1
         if currSteps > n:
             return 0
-        numWays = self.dfs(n, currSteps+1, numWays) + self.dfs(n, currSteps+2, numWays)
+        numWays = self.dfs(n, currSteps+1, numWays, memo) + self.dfs(n, currSteps+2, numWays, memo)
+        if currSteps not in memo:
+            memo[currSteps] = numWays
         return numWays
      
